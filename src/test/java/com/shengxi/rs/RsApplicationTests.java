63d3f5004001dabb48b8eb76b4299b727bcf34c0
matthew <matthewvictorialeung@outlook.com>
2019-03-14 21:34:16 +0800

    配置log4j

--- src/test/java/com/shengxi/rs/RsApplicationTests.java
+++ src/test/java/com/shengxi/rs/RsApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class RsApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+
+	@Test
+	public void test(){
+		String time = "19-03-10";
+		System.out.println(time.substring(6,8));
+	}
 }
---
+++ src/test/java/com/shengxi/rs/controller/test/TestTest.java
@@ -0,0 +1,17 @@
+package com.shengxi.rs.controller.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Auther: Matthew
+ * @Date: 2019/3/14 20:48
+ * @Description:
+ */
+public class TestTest {
+
+    @Test
+    public void test1() {
+    }
+}
