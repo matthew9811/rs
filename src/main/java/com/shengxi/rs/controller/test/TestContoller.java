58aa5271ef9302bd7b0322a2b602dffeacd631b7
matthew <matthewvictorialeung@outlook.com>
2019-03-23 20:41:06 +0800

    test页面跳转，表单提交

--- src/main/java/com/shengxi/rs/controller/test/TestContoller.java
+++ src/main/java/com/shengxi/rs/controller/test/TestContoller.java
@@ -1,7 +1,11 @@
 package com.shengxi.rs.controller.test;
 
+import com.shengxi.system.entites.test.TestEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author: Matthew
@@ -14,7 +18,19 @@ public class TestContoller {
     private String prifix = "test";
 
     @RequestMapping("")
-    public String test(){
+    public String test() {
         return prifix + "/index";
+    }
+
+    @GetMapping("/input")
+    public String input() {
+        return prifix + "/inputTest";
+    }
+
+    @PostMapping("/input")
+    @ResponseBody
+    public String input(TestEntity entity) {
+        System.out.println(entity.toString());
+        return entity.toString();
     }
 }
