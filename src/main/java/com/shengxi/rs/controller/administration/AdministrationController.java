94af143b56dbfeefaea472d615f368ee4e9f40a2
zhengjei <2501178392@qq.com>
2019-03-25 11:13:35 +0800

    修改了开课学院，所属学院页面排版，增加了教务审批页及跳转

---
+++ src/main/java/com/shengxi/rs/controller/administration/AdministrationController.java
@@ -0,0 +1,24 @@
+package com.shengxi.rs.controller.administration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 教务审批跳转
+ *
+ * @author 郑杰
+ * @date  2019/3/25 10:25
+ * 教务审批跳转
+ */
+@Controller
+@RequestMapping("/administration/administration")
+public class AdministrationController {
+
+    private  String prifix = "administration";
+
+    @GetMapping("/approval")
+    public String studentAccept() {
+        return prifix + "/approval";
+    }
+}
