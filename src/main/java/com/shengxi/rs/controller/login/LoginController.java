783bca39809fa6598276b21875f39bc2bcf91767
zhengjei <2501178392@qq.com>
2019-03-27 22:30:35 +0800

    修改学院/教务审批页，增加登录页，增加登录判断

---
+++ src/main/java/com/shengxi/rs/controller/login/LoginController.java
@@ -0,0 +1,45 @@
+package com.shengxi.rs.controller.login;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.regex.Pattern;
+
+
+/**
+ * 登录表接受数据跳转
+ *
+ * @author 郑杰
+ * @date 2019/3/27 16:37
+ * 登录表接受数据跳转
+ */
+@Controller
+@RequestMapping("/login/login")
+public class LoginController {
+
+    private Pattern pattern = Pattern.compile("[0-9]*");
+    private String prifix = "login";
+
+    @GetMapping("/login")
+    public String login() {
+        return  prifix + "/login";
+    }
+
+@RequestMapping(value = "/judge", method = RequestMethod.POST)
+    public String judge(@RequestParam("id") String id) {
+
+        if (id.startsWith("neusoft",0)) {
+            return "redirect:/administration/administration/approval";
+        }
+        if (id.startsWith("CST",0)){
+            return "redirect:/college/college/classbegins";
+        }
+        if (pattern.matcher(id).matches()){
+            return "redirect:/stu/stu/stuAccept";
+        }
+    return "redirect:/test/index";
+}
+}
