f6ab9d0a593c8145087c7806c5dcd9d91d8f9fc4
matthew <matthewvictorialeung@outlook.com>
2019-03-28 17:08:29 +0800

    优化日志打印格式，增加model层实例

---
+++ src/main/java/com/shengxi/system/model/TestServices.java
@@ -0,0 +1,17 @@
+package com.shengxi.system.model;
+
+import com.shengxi.system.entites.test.TestEntity;
+
+import java.util.List;
+
+/**
+ * @author : Matthew
+ * @Date: 2019/3/28 16:58
+ * @Description: 服务层模版
+ */
+public class TestServices {
+
+    List<TestEntity> selectList(){
+        return null;
+    }
+}
