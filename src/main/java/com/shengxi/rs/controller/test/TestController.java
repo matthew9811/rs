d8f4951debc5316239044ff888d9f764e88ce72d
matthew <matthewvictorialeung@outlook.com>
2019-05-09 17:12:34 +0800

    实现layui table初步实现

--- src/main/java/com/shengxi/rs/controller/test/TestController.java
+++ src/main/java/com/shengxi/rs/controller/test/TestController.java
@@ -1,7 +1,9 @@
 package com.shengxi.rs.controller.test;
 
+import afu.org.checkerframework.checker.oigj.qual.O;
 import com.shengxi.rs.controller.sys.BaseController;
 import com.shengxi.system.entites.test.TestEntity;
+import com.shengxi.system.entites.test.TestStu;
 import com.shengxi.system.model.service.test.TestServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: Matthew
@@ -19,7 +24,7 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/test")
-public class TestContoller extends BaseController {
+public class TestController extends BaseController {
     private String prefix = "test";
 
     @Autowired
@@ -45,8 +50,27 @@ public class TestContoller extends BaseController {
     }
 
     @GetMapping("/main")
-    public String main(){
+    public String main() {
         return "main";
+    }
+
+    @RequestMapping("/getList")
+    @ResponseBody
+    public Map<String, Object> getList() {
+        List<TestStu> list = new ArrayList<>();
+        TestStu stu = new TestStu();
+        stu.setSubNo("CS1007");
+        stu.setSubName("线性代数");
+        stu.setTerm("2017-2018-1");
+        stu.setChoiceStatus("0");
+        stu.setGrade(59.5);
+        list.add(stu);
+        Map<String, Object> map = new HashMap<>();
+        map.put("code","0");
+        map.put("msg", "200");
+        map.put("count", 5);
+        map.put("data", list);
+        return map;
     }
 
 
---
+++ src/main/java/com/shengxi/system/entites/test/TestStu.java
@@ -0,0 +1,56 @@
+package com.shengxi.system.entites.test;
+
+import com.shengxi.rs.common.domain.BaseEntity;
+
+/**
+ * @author: Matthew
+ * @Date: 2019/5/9 16:09
+ * @Description:
+ */
+public class TestStu  extends BaseEntity {
+    private String subNo;
+    private String subName;
+    private String term;
+    private Double grade;
+    private String choiceStatus;
+
+    public String getChoiceStatus() {
+        return choiceStatus;
+    }
+
+    public void setChoiceStatus(String choiceStatus) {
+        this.choiceStatus = choiceStatus;
+    }
+
+    public String getSubNo() {
+        return subNo;
+    }
+
+    public void setSubNo(String subNo) {
+        this.subNo = subNo;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+}
