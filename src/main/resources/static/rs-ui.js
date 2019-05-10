94fccc56f9887e5dfd0a70c0acda610fcef12826
楚苓 <97533612152@qq.com>
2019-05-10 14:22:48 +0800

    修改layer文件。增加部分layer table的功能

--- src/main/resources/static/rs-ui.js
+++ src/main/resources/static/rs-ui.js
@@ -5,23 +5,23 @@
     /*jq扩展函数直接包含在内部的代码能直接使用jq扩展*/
     $.extend({
     //layer-table 模块
-    table: {
-        init: function(options){
-        var defaults = {
-        title: '',
-        url: ''
-        };
-        //传入两个对象。返回合并的值
-        //defaults没有的属性，会增加
-        //options有的会覆盖defaults
-        var options = $.extend(defaults, options);
-        var table = layer.table;
-        table.render({
-            title : options.title,
-            url: options.url,
-        })
-        };
-    },
+        table: {
+            init: function(options){
+                var defaults = {
+                    title: '',
+                    url: ''
+                };
+                //传入两个对象。返回合并的值
+                //defaults没有的属性，会增加
+                //options有的会覆盖defaults
+                var options = $.extend(defaults, options);
+                var table = layer.table;
+                table.render({
+                    title : options.title,
+                    url: options.url,
+                })
+            };
+        },
     })
 })(jQuery);
 
