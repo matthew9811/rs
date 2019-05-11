0a06eb8baaf7ab720b8405770862928b432eb032
楚苓 <97533612152@qq.com>
2019-05-11 20:37:35 +0800

    增加pageController

---
+++ src/main/java/com/shengxi/rs/controller/test/PageController.java
@@ -0,0 +1,28 @@
+package com.shengxi.rs.controller.test;
+
+import com.shengxi.rs.controller.sys.BaseController;
+import com.shengxi.system.model.service.test.TestServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @date 2019.05.12
+ * @author chuling
+ */
+@RequestMapping("/test/page")
+@Controller
+public class PageController extends BaseController {
+
+    @Autowired
+    private TestServices testServices;
+
+
+    @GetMapping("/index")
+    public String jumpIndex(){
+        return "/index";
+    }
+
+}
