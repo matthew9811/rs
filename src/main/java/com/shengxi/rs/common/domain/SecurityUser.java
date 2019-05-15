a436120426f30fc5dc687175b5b43cc638388b97
matthew <matthewvictorialeung@outlook.com>
2019-05-15 10:59:29 +0800

    修改课程sql

--- src/main/java/com/shengxi/rs/common/domain/SecurityUser.java
+++ src/main/java/com/shengxi/rs/common/domain/SecurityUser.java
@@ -1,9 +1,15 @@
 package com.shengxi.rs.common.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.shengxi.system.entites.sys.SysMenuEntity;
 import com.shengxi.system.entites.sys.SysUser;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.ObjectUtils;
 
 /**
  * @author : Matthew
@@ -12,13 +18,23 @@ import org.springframework.security.core.userdetails.UserDetails;
  */
 public class SecurityUser extends SysUser implements UserDetails {
 
+    /**
+     * 权限
+     */
+    private List<SysMenuEntity> permList;
+
     public SecurityUser() {
         super();
     }
 
+    /**
+     * 获取菜单权限
+     *
+     * @return list
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return permList.parallelStream().filter(p -> !ObjectUtils.isEmpty(p.getPerms())).map(p -> new SimpleGrantedAuthority(p.getPerms())).collect(Collectors.toSet());
     }
 
     @Override
@@ -26,23 +42,45 @@ public class SecurityUser extends SysUser implements UserDetails {
         return getUserNo();
     }
 
+
+    /**
+     * 用户是否锁定
+     * @return boolean
+     */
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
+    /**
+     * 密码是否过期
+     * @return boolean
+     */
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
+    /**
+     * 用户是否激活
+     * @return boolean
+     */
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return false;
+    }
+
+    public List<SysMenuEntity> getPermList() {
+        return permList;
+    }
+
+    public void setPermList(List<SysMenuEntity> permList) {
+        this.permList = permList;
     }
 }
