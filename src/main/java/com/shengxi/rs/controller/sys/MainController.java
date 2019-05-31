444d579dc4ce72ce468729ae3e27a47ef0271290
matthew <matthewvictorialeung@outlook.com>
2019-05-31 11:06:35 +0800

    修改登录配置

--- src/main/java/com/shengxi/rs/controller/sys/MainController.java
+++ src/main/java/com/shengxi/rs/controller/sys/MainController.java
@@ -1,6 +1,8 @@
 package com.shengxi.rs.controller.sys;
 
 import com.shengxi.rs.common.handler.BaseController;
+import javax.annotation.security.PermitAll;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/shengxi/rs/controller/sys/UserController.java
+++ src/main/java/com/shengxi/rs/controller/sys/UserController.java
@@ -1,15 +1,19 @@
 package com.shengxi.rs.controller.sys;
 
 import com.shengxi.rs.common.handler.BaseController;
+import com.shengxi.rs.common.util.TokenUtil;
 import com.shengxi.rs.common.util.UserUtil;
 import com.shengxi.system.entites.sys.SysUser;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 用户管理控制器
+ *
  * @author matthew
  * @date 2019/05/19
  */
@@ -19,14 +23,14 @@ public class UserController extends BaseController {
 
     private String prefix = "/admin/user";
 
-    @GetMapping("/current")
+    @GetMapping("/current/{token}")
     @ResponseBody
-    public SysUser currentUser() {
-        return UserUtil.getLoginUser();
+    public SysUser currentUser(@PathVariable("token") String token) {
+        return TokenUtil.getUser(token);
     }
 
     @GetMapping("")
-    public String index(){
+    public String index() {
         return prefix + "/user";
     }
 }
